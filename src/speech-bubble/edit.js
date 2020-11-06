c03603de091123914759c69614a5812a64cca84d
Hiroshi Urabe <urabe@ham.works>
2020-11-06 20:54:33 +0900

    fix attr. support caption.

--- src/speech-bubble/edit.js
+++ src/speech-bubble/edit.js
@@ -3,20 +3,34 @@ import {
 	BlockControls,
 	MediaReplaceFlow,
 	InnerBlocks,
+	RichText,
+	PanelColorSettings,
+	withColors,
+	InspectorControls,
+	ContrastChecker,
 } from '@wordpress/block-editor';
+import classnames from 'classnames';
 import { pullLeft, pullRight } from '@wordpress/icons';
 import { __ } from '@wordpress/i18n';
 import { ToolbarGroup } from '@wordpress/components';
 
 const Edit = ( {
 	className,
-	attributes: { mediaId, mediaURL, mediaPosition },
+	attributes: { mediaId, mediaURL, mediaPosition, mediaCaption },
 	setAttributes,
+	backgroundColor,
+	setBackgroundColor,
+	borderColor,
+	setBorderColor,
+	textColor,
+	setTextColor,
 } ) => {
-	const onSelectMedia = ( { id, url } ) => {
+	const onSelectMedia = ( props ) => {
+		const { id, url, caption, alt } = props;
 		setAttributes( {
 			mediaId: id,
 			mediaURL: url,
+			mediaCaption: caption || alt || '',
 		} );
 	};
 
@@ -45,7 +59,28 @@ const Edit = ( {
 		},
 	];
 
-	const newClassName = `${ className } has-media-on-the-${ mediaPosition }`;
+	const newBlockProps = {
+		style: {
+			'--bubble-bg-color': backgroundColor.color,
+			'--bubble-bd-color': borderColor.color,
+			color: textColor.color,
+		},
+		className: classnames( className, {
+			[ `has-media-on-the-${ mediaPosition }` ]: mediaPosition,
+		} ),
+	};
+
+	const commentClassName = classnames(
+		`wp-block-simple-speech-bubble-speech-bubble__comment`,
+		{
+			'has-background-color': backgroundColor.color,
+			[ backgroundColor.class ]: backgroundColor.class,
+			'has-border-color': borderColor.color,
+			[ borderColor.class ]: borderColor.class,
+			'has-text-color': textColor.color,
+			[ textColor.class ]: textColor.class,
+		}
+	);
 
 	return (
 		<>
@@ -59,16 +94,67 @@ const Edit = ( {
 					onSelect={ onSelectMedia }
 				/>
 			</BlockControls>
-			<div className={ newClassName }>
-				<div className="wp-block-simple-speech-bubble-speech-bubble__media">
+			<InspectorControls>
+				<PanelColorSettings
+					title={ __( 'Color settings' ) }
+					colorSettings={ [
+						{
+							value: backgroundColor.color,
+							onChange: setBackgroundColor,
+							label: __( 'Background color' ),
+						},
+						{
+							value: textColor.color,
+							onChange: setTextColor,
+							label: __( 'Text color' ),
+						},
+						{
+							value: borderColor.color,
+							onChange: setBorderColor,
+							label: __( 'Border color' ),
+						},
+					] }
+				>
+					<ContrastChecker
+						{ ...{
+							textColor: textColor.color,
+							backgroundColor: backgroundColor.color,
+						} }
+						isLargeText={ false }
+					/>
+				</PanelColorSettings>
+			</InspectorControls>
+			<div
+				className={ newBlockProps.className }
+				style={ newBlockProps.style }
+			>
+				<figure className="wp-block-simple-speech-bubble-speech-bubble__media">
 					{ media }
-				</div>
-				<div className="wp-block-simple-speech-bubble-speech-bubble__comment">
-					<InnerBlocks />
+					<RichText
+						tagName="figcaption"
+						value={ mediaCaption }
+						onChange={ ( content ) =>
+							setAttributes( { mediaCaption: content } )
+						}
+					/>
+				</figure>
+				<div className={ commentClassName }>
+					<InnerBlocks
+						template={ [
+							[
+								'core/paragraph',
+								{ placeholder: 'Enter side content...' },
+							],
+						] }
+					/>
 				</div>
 			</div>
 		</>
 	);
 };
 
-export default Edit;
+export default withColors( {
+	backgroundColor: 'background-color',
+	borderColor: 'border-color',
+	textColor: 'text-color',
+} )( Edit );
--- src/speech-bubble/save.js
+++ src/speech-bubble/save.js
@@ -1,12 +1,18 @@
-import { InnerBlocks } from '@wordpress/block-editor';
+import { InnerBlocks, RichText } from '@wordpress/block-editor';
 
-const Save = ( { attributes: { mediaURL, mediaPosition } } ) => {
+const Save = ( { attributes: { mediaURL, mediaPosition, mediaCaption } } ) => {
 	const newClassName = `wp-block-simple-speech-bubble-speech-bubble has-media-on-the-${ mediaPosition }`;
 
 	return (
 		<div className={ newClassName }>
 			<figure className="wp-block-simple-speech-bubble-speech-bubble__media">
 				<img src={ mediaURL } alt="" />
+				{ mediaCaption ? (
+					<RichText.Content
+						tagName="figcaption"
+						value={ mediaCaption }
+					/>
+				) : null }
 			</figure>
 			<InnerBlocks.Content />
 		</div>
