92cf44dbdf484fd2ab90cf2ae1d9ca44a8fdec0c
Hiroshi Urabe <urabe@ham.works>
2020-11-09 18:26:57 +0900

    add plugin headers

--- simple-speech-bubble.php
+++ simple-speech-bubble.php
@@ -1,6 +1,15 @@
 <?php
 /**
- * Plugin Name: Simple Speech Bubble.
+ * Plugin Name:     Simple Speech Bubble.
+ * Plugin URI:      https://github.com/team-hamworks/editors-note
+ * Description:     Simple Speech Bubble Block.
+ * Author:          HAMWORKS
+ * Author URI:      https://ham.works
+ * License:         GPLv2 or later
+ * License URI:     https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:     simple-speech-bubble
+ * Domain Path:     /languages
+ * Version: 0.0.1
  */
 
 add_action(
