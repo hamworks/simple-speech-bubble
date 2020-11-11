ad619d7aca848a7e40e1a643b9eac3eb18162909
Hiroshi Urabe <urabe@ham.works>
2020-11-11 16:33:27 +0900

    Release 0.0.3

--- simple-speech-bubble.php
+++ simple-speech-bubble.php
@@ -9,7 +9,7 @@
  * License URI:     https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:     simple-speech-bubble
  * Domain Path:     /languages
- * Version: 0.0.2
+ * Version: 0.0.3
  */
 
 add_action(
